1a7b40466899b8d50f0bb7c3c6cec881f613572f
VinayPawar1803 <86231811+VinayPawar1803@users.noreply.github.com>
2021-06-28 17:38:16 +0530

    Add files via upload

---
+++ MadLibsGenerator.py
@@ -0,0 +1,18 @@
+loop = 1
+while (loop < 10):
+    noun = input("Choose a noun:anger ")
+    p_noun = input("Choose a plural noun:person ")
+    noun2 = input("Choose a noun:boy ")
+    place = input("Name a place:school ")
+    adjective = input("Choose an adjective (Describing word):rohit is angry ")
+    noun3 = input("Choose a noun:arrow ")
+    print ("------------------------------------------")
+    print ("Be kind to your",anger,"- footed", person)
+    print ("For a duck may be somebody's", boy,",")
+    print ("Be kind to your",person,"in",school)
+    print ("Where the weather is always",rohit is angry,".")
+    print ()
+    print ("You may think that is this the",arrow,",")
+    print ("Well it is.")
+    print ("------------------------------------------")
+    loop = loop + 1
